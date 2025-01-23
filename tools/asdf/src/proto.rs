6a53c12e7193e6285fcbe635a59af5a4c78cd27b
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2025-01-23 14:14:33 -0500

    Use the new builder API

--- tools/asdf/src/proto.rs
+++ tools/asdf/src/proto.rs
@@ -2,13 +2,12 @@ use crate::config::AsdfPluginConfig;
 use extism_pdk::*;
 use json::Value;
 use proto_pdk::*;
-use std::{collections::HashMap, fs};
+use std::collections::HashMap;
+use std::fs;
 
 #[host_fn]
 extern "ExtismHost" {
     fn exec_command(input: Json<ExecCommandInput>) -> Json<ExecCommandOutput>;
-    fn get_env_var(key: &str) -> String;
-    fn set_env_var(name: String, value: String);
     fn send_request(input: Json<SendRequestInput>) -> Json<SendRequestOutput>;
     fn from_virtual_path(path: String) -> String;
     fn to_virtual_path(path: String) -> String;
@@ -21,7 +20,7 @@ fn is_asdf_repo(config: &AsdfPluginConfig) -> bool {
     config.asdf_repository.is_some()
 }
 
-fn get_raw_github_url(repo: &str) -> FnResult<String> {
+fn get_default_branch(repo: &str) -> FnResult<String> {
     let repo_parts: Vec<&str> = repo.split("/").collect();
 
     // Get the default branch of the repository
@@ -33,21 +32,16 @@ fn get_raw_github_url(repo: &str) -> FnResult<String> {
                 None => &Value::String(String::from("main"))
             };
             let default_branch = default_branch.as_str().unwrap();
-            let repo = repo.replace("https://github.com", "https://raw.githubusercontent.com");
-
-            return Ok(format!("{repo}/refs/heads/{default_branch}"));
+            return Ok(default_branch.into());
         },
         _ => Err(PluginError::Message("Failed to fetch repository's default branch".to_string()).into())
     }
 }
 
-fn get_script_url(script: &str, git: bool) -> FnResult<String> {
+fn get_repo_url() -> FnResult<String> {
     let config = get_tool_config::<AsdfPluginConfig>()?;
     if is_asdf_repo(&config) {
-        if git {
-            return Ok(config.asdf_repository.unwrap().trim().to_string());
-        }
-        return Ok(format!("{}/bin/{script}", get_raw_github_url(config.asdf_repository.unwrap().as_str().split(".git").next().unwrap())?));
+        return Ok(config.asdf_repository.unwrap().trim().to_string());
     }
 
     let id = get_plugin_id()?;
@@ -67,49 +61,41 @@ fn get_script_url(script: &str, git: bool) -> FnResult<String> {
         .into());
     };
 
-    if git {
-        return Ok(repo.trim().to_string());
-    }
-
     let repo = repo.split(".git").next();
     let Some(repo) = repo else {
         return Err(PluginError::Message(String::from("Repository not found in downloaded file"))
         .into());
     };
 
-    Ok(format!("{}/bin/{script}", get_raw_github_url(repo)?))
+    Ok(repo.into())
 }
 
-/// Returns the real path of the script
-fn get_script(script: &str) -> FnResult<String> {
+fn get_versions() -> FnResult<Vec<String>> {
     let id = get_plugin_id()?;
 
-    let virtual_script_path = virtual_path!(format!("/proto/temp/{id}"));
-    let script_url = get_script_url(script, true)?;
-    let repo_dir = virtual_script_path.join("asdf");
-    
-    fs::create_dir_all(virtual_script_path.parent().unwrap())?;
-    exec_command!("rm", ["-rf", repo_dir.real_path().unwrap().to_str().unwrap()]);
-    exec_command!("git", ["clone", "--depth=1", &script_url, repo_dir.real_path().unwrap().to_str().unwrap()]);
+    let script_dir = virtual_path!(format!("/proto/temp/{id}"));
+    let virtual_script_path = script_dir.join("list-all");
+    let script_path = virtual_script_path.real_path().unwrap();
+    let script_path = script_path.to_str().unwrap();
+    let repo = get_repo_url()?;
+    let default_branch = get_default_branch(&repo)?;
+    let script_url = format!(
+        "{}/refs/heads/{default_branch}/bin/list-all",
+        repo.replace("https://github.com", "https://raw.githubusercontent.com")
+    );
 
-    let real_script_path = virtual_script_path.join("asdf").join("bin").real_path().unwrap().into_os_string().into_string().unwrap();
-    Ok(format!("{real_script_path}/{script}"))
-}
+    let clean  = || {
+        Ok::<i32, Error>(exec_command!("rm", ["-rf", script_dir.real_path().unwrap().to_str().unwrap()]).exit_code)
+    };
 
-/// Set the environment variables to be used by asdf scripts
-fn set_asdf_env_vars(version: &str) -> FnResult<()> {
-    host_env!("ASDF_INSTALL_TYPE", "version");
-    host_env!("ASDF_INSTALL_VERSION", version);
+    clean()?;
+    fs::create_dir_all(&script_dir)?;
+    fs::write(&virtual_script_path, fetch_text(script_url)?)?;
+    let versions = exec_command!("bash", [script_path]).stdout;
+    let versions: Vec<String> = versions.split_whitespace().map(str::to_owned).collect();
+    clean()?;
 
-    let asdf_bin = virtual_path!("/proto/bin/asdf");
-    host_env!("ASDF_INSTALL_PATH", asdf_bin.real_path().unwrap().to_str().unwrap());
-    asdf_bin.real_path().unwrap().to_str().unwrap();
-    host_env!("ASDF_DOWNLOAD_PATH", asdf_bin.real_path().unwrap().to_str().unwrap());
-
-    let cores = exec_command!("nproc").stdout;
-    host_env!("ASDF_CONCURRENCY", cores);
-
-    Ok(())
+    Ok(versions)
 }
 
 #[plugin_fn]
@@ -117,7 +103,7 @@ pub fn register_tool(Json(input): Json<ToolMetadataInput>) -> FnResult<Json<Tool
     Ok(Json(ToolMetadataOutput {
         name: input.id,
         type_of: PluginType::Language,
-        minimum_proto_version: Some(Version::new(0, 44, 7)),
+        minimum_proto_version: Some(Version::new(0, 45, 0)),
         plugin_version: Version::parse(env!("CARGO_PKG_VERSION")).ok(),
 		config_schema: Some(schematic::SchemaBuilder::generate::<AsdfPluginConfig>()),
         ..ToolMetadataOutput::default()
@@ -125,71 +111,98 @@ pub fn register_tool(Json(input): Json<ToolMetadataInput>) -> FnResult<Json<Tool
 }
 
 #[plugin_fn]
-pub fn download_prebuilt(Json(_): Json<DownloadPrebuiltInput>) -> FnResult<Json<DownloadPrebuiltOutput>> {
-    let env = get_host_environment()?;
-	let id = get_plugin_id()?;
+pub fn build_instructions(
+    Json(input): Json<BuildInstructionsInput>,
+) -> FnResult<Json<BuildInstructionsOutput>> {
+    let env = get_host_environment()?;    
 
-    check_supported_os_and_arch(
-        &id,
-        &env,
-        permutations! [
-            HostOS::Linux => [HostArch::X64, HostArch::Arm64, HostArch::Arm, HostArch::Powerpc64, HostArch::S390x],
-            HostOS::MacOS => [HostArch::X64, HostArch::Arm64],
-        ],
-    )?;
-    
-    Ok(Json(DownloadPrebuiltOutput {
-        archive_prefix: Some(String::from("/asdf/temp")),
-        download_url: get_script_url("install", false)?,
-        download_name: Some(String::from("list-all")),
-        ..DownloadPrebuiltOutput::default()
-    }))
-}
+    if env.os.is_windows() {
+        return Err(PluginError::UnsupportedWindowsBuild.into());
+    }
 
-#[plugin_fn]
-pub fn unpack_archive(Json(input): Json<UnpackArchiveInput>) -> FnResult<()> {
-    set_asdf_env_vars(input.context.version.to_string().as_str())?;
+    let repo = get_repo_url()?;
+    let download_script = fetch_text(format!("{repo}/bin/download"));
 
-    let download_script = get_script("download");
+    let git = GitSource {
+        url: repo.clone(),
+        // Use default branch
+        reference: None,
+        ..Default::default()
+    };
+
+    let mut instructions = Vec::new();
+
+    // Set asdf environment variables
+    let install_download_path = input.context.tool_dir.real_path().unwrap().into_os_string().into_string().unwrap();
+    let cores = if env.os.is_mac() {
+        exec_command!("sysctl -n hw.physicalcpu").stdout
+    } else {
+        exec_command!("nproc").stdout
+    };
+    instructions.append(&mut vec![
+        BuildInstruction::SetEnvVar("ASDF_INSTALL_TYPE".into(), "version".into()),
+        BuildInstruction::SetEnvVar("ASDF_INSTALL_VERSION".into(), input.context.version.into()),
+        BuildInstruction::SetEnvVar("ASDF_INSTALL_PATH".into(), install_download_path.clone()),
+        BuildInstruction::SetEnvVar("ASDF_DOWNLOAD_PATH".into(), install_download_path.clone()),
+        BuildInstruction::SetEnvVar("ASDF_CONCURRENCY".into(), cores)
+    ]);
+
     // In older versions of asdf there may not be a 'download' script,
     // instead both download and install were done in the 'install' script.
     // However, in newer versions, there's two separate 'download' and 'install' scripts.
     match download_script {
-        // Newer versions
-        Ok(download_script) => {
-            exec_command!("bash", [download_script]).stdout;
+        Ok(_) => {
+            let download_id = String::from("download");
+            instructions.push(BuildInstruction::InstallBuilder(Box::new(BuilderInstruction {
+                id: download_id.clone(),
+                exe: "bin/download".into(),
+                git: git.clone()
+            })));
+            
+            instructions.push(
+                BuildInstruction::RunCommand(Box::new(CommandInstruction::with_builder(
+                    &download_id,
+                    [""],
+                ))),
+            )
+        },
+        _ => ()
+    };
 
-            // Install script was already downloaded by the `download_prebuilt` function
-            let install_script = get_script("install")?;
-            let install_script = install_script.as_str();
-            exec_command!("bash", [install_script]);
-        }
-        // Older versions
-        Err(_) => {
-            let download_and_install_script = get_script("install")?;
-            exec_command!("bash", [download_and_install_script]);
-        }
-    }
+    let install_id = String::from("install");
+    instructions.push(
+        BuildInstruction::InstallBuilder(Box::new(BuilderInstruction {
+            id: install_id.clone(),
+            exe: "bin/install".into(),
+            git,
+        }))
+    );
+    instructions.push(
+        BuildInstruction::RunCommand(Box::new(CommandInstruction::with_builder(
+            &install_id,
+            [""],
+        ))),
+    );
 
-    Ok(())
+    let output = BuildInstructionsOutput {
+        instructions,
+        ..Default::default()
+    };
+
+    Ok(Json(output))
 }
 
 #[plugin_fn]
 pub fn locate_executables(
     Json(input): Json<LocateExecutablesInput>,
 ) -> FnResult<Json<LocateExecutablesOutput>> {
-    set_asdf_env_vars(input.context.version.to_string().as_str())?;
-
     let config = get_tool_config::<AsdfPluginConfig>()?;
     let id = match config.asdf_shortname {
         Some(shortname) => shortname,
         None => get_plugin_id()?,
     };
-    let install_path = host_env!("ASDF_INSTALL_PATH");
-    let Some(install_path) = install_path else {
-        return Err(PluginError::Message("ASDF_DOWNLOAD_PATH environment variable not found".to_string()).into());
-    };
 
+    let install_path = input.context.tool_dir.real_path().unwrap().into_os_string().into_string().unwrap();
     Ok(Json(LocateExecutablesOutput {
         exes: HashMap::from_iter([
             (
@@ -208,12 +221,11 @@ pub fn locate_executables(
 pub fn load_versions(Json(_): Json<LoadVersionsInput>) -> FnResult<Json<LoadVersionsOutput>> {
     let mut output = LoadVersionsOutput::default();
 
-    let versions = exec_command!("bash", [get_script("list-all")?]).stdout;
-    let mut versions: Vec<&str> = versions.split_whitespace().map(AsRef::as_ref).collect();
-    
+    let mut versions = get_versions()?;
      // Remove the last element, which is the latest version
     let last_version = versions.pop().unwrap();
-    let version = UnresolvedVersionSpec::parse(last_version);
+    let version = UnresolvedVersionSpec::parse(&last_version);
+
     match version {
         Ok(version) => {
             output.latest = Some(version);
