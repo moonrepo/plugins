d3904cf0b195c28f90f859dfe6d0a766bde36d2e
Miles Johnson <mileswjohnson@gmail.com>
2025-02-23 12:20:43 -0800

    Move functionality into config.

---
+++ backends/asdf/README.md
@@ -0,0 +1,46 @@
+# asdf plugin
+
+[asdf](https://asdf-vm.com/) backend WASM plugin for [proto](https://github.com/moonrepo/proto).
+
+## Installation
+
+This plugin is built-in to proto, but if you want to override it with an explicit version, add the following to `.prototools`.
+
+```toml
+[plugins]
+asdf = "https://github.com/moonrepo/plugins/releases/download/asdf_backend-vX.Y.Z/asdf_backend.wasm"
+```
+
+## Configuration
+
+asdf plugin can be configured with a `.prototools` file.
+
+- `asdf-shortname` (string) - The name of the [asdf plugin](https://github.com/asdf-vm/asdf-plugins) if different than the configured ID.
+- `asdf-repository` (string) - The Git repository URL in which to locate [scripts](https://asdf-vm.com/plugins/create.html#scripts-overview). If not defined, is extracted from the shortname plugin manifest.
+- `executable-name` (string) - The name of the executable if different from the configured ID.
+
+```toml
+<id> = "asdf:1.2.3"
+
+[tools.<id>]
+asdf-shortname = "..."
+```
+
+## Hooks
+
+asdf plugin does not support hooks.
+
+## Contributing
+
+Build the plugin:
+
+```shell
+cargo build --target wasm32-wasip1
+```
+
+Test the plugin by running `proto` commands.
+
+```shell
+proto install asdf-test
+proto versions asdf-test
+```
--- backends/asdf/src/config.rs
+++ backends/asdf/src/config.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+
+use extism_pdk::*;
+use proto_pdk::*;
+
+#[host_fn]
+extern "ExtismHost" {
+    // fn exec_command(input: Json<ExecCommandInput>) -> Json<ExecCommandOutput>;
+    fn send_request(input: Json<SendRequestInput>) -> Json<SendRequestOutput>;
+    // fn from_virtual_path(path: String) -> String;
+    // fn to_virtual_path(path: String) -> String;
+    // fn host_log(input: Json<HostLogInput>);
+}
+
+const ASDF_PLUGINS_URL: &str =
+    "https://raw.githubusercontent.com/asdf-vm/asdf-plugins/refs/heads/master/plugins";
+
 /// https://asdf-vm.com/manage/plugins.html
 #[derive(Debug, Default, schematic::Schematic, serde::Deserialize, serde::Serialize)]
 #[serde(default, deny_unknown_fields, rename_all = "kebab-case")]
@@ -5,4 +22,56 @@ pub struct AsdfPluginConfig {
     pub asdf_shortname: Option<String>,
     pub asdf_repository: Option<String>,
     pub executable_name: Option<String>,
+}
+
+impl AsdfPluginConfig {
+    pub fn get_exe_name(&self) -> AnyResult<String> {
+        match &self.executable_name {
+            Some(name) => Ok(name.into()),
+            None => self.get_id(),
+        }
+    }
+
+    pub fn get_id(&self) -> AnyResult<String> {
+        match &self.asdf_shortname {
+            Some(name) => Ok(name.into()),
+            None => get_plugin_id(),
+        }
+    }
+
+    pub fn get_backend_id(&self) -> AnyResult<String> {
+        Ok(format!("asdf-{}", self.get_id()?))
+    }
+
+    pub fn get_backend_path(&self) -> AnyResult<PathBuf> {
+        let backend_id = self.get_backend_id()?;
+
+        Ok(PathBuf::from(format!("/proto/backends/{backend_id}")))
+    }
+
+    pub fn get_repo_url(&self) -> AnyResult<String> {
+        if let Some(repo_url) = &self.asdf_repository {
+            return Ok(repo_url.into());
+        }
+
+        let id = self.get_id()?;
+        let filepath = format!("{ASDF_PLUGINS_URL}/{id}");
+        let repo_response = send_request!(&filepath);
+
+        let repo_config = match repo_response.status {
+            200 => Ok::<String, Error>(repo_response.text()?),
+            404 => Err(PluginError::Message(format!("URL not found: {filepath}")).into()),
+            _ => Err(PluginError::Message(format!("Failed to fetch URL: {filepath}")).into()),
+        }?;
+        let repo_config = repo_config.replace(" ", "");
+
+        let Some(repo_url) = repo_config.split("=").last() else {
+            return Err(PluginError::Message(String::from(
+                "Repository not found in downloaded file!",
+            ))
+            .into());
+        };
+
+        Ok(repo_url.trim_end_matches(".git").into())
+    }
 }
