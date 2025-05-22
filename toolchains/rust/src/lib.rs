a48bf713ec819e0c4e3e047be570b2c9aa258f64
Miles Johnson <mileswjohnson@gmail.com>
2025-05-22 15:22:38 -0700

    Move to tier 1.

--- toolchains/rust/src/lib.rs
+++ toolchains/rust/src/lib.rs
@@ -4,8 +4,6 @@ mod config;
 pub mod toolchain_toml;
 
 #[cfg(feature = "wasm")]
-mod docker;
-#[cfg(feature = "wasm")]
 mod install_deps;
 #[cfg(feature = "wasm")]
 mod moon;
@@ -13,9 +11,11 @@ mod moon;
 mod run_task;
 #[cfg(feature = "wasm")]
 mod setup_env;
-
 #[cfg(feature = "wasm")]
-pub use docker::*;
+mod tier1;
+#[cfg(feature = "wasm")]
+mod tier3;
+
 #[cfg(feature = "wasm")]
 pub use install_deps::*;
 #[cfg(feature = "wasm")]
@@ -23,6 +23,8 @@ pub use moon::*;
 #[cfg(feature = "wasm")]
 pub use run_task::*;
 #[cfg(feature = "wasm")]
-pub use rust_tool::*;
-#[cfg(feature = "wasm")]
 pub use setup_env::*;
+#[cfg(feature = "wasm")]
+pub use tier1::*;
+#[cfg(feature = "wasm")]
+pub use tier3::*;
--- toolchains/rust/src/moon.rs
+++ toolchains/rust/src/moon.rs
@@ -7,51 +7,6 @@ use moon_pdk_api::*;
 use schematic::SchemaBuilder;
 
 #[plugin_fn]
-pub fn register_toolchain(
-    Json(_): Json<RegisterToolchainInput>,
-) -> FnResult<Json<RegisterToolchainOutput>> {
-    Ok(Json(RegisterToolchainOutput {
-        name: "Rust".into(),
-        plugin_version: env!("CARGO_PKG_VERSION").into(),
-        config_file_globs: vec![
-            ".cargo/*.toml".into(),
-            "rust-toolchain".into(),
-            "rust-toolchain.toml".into(),
-        ],
-        exe_names: vec![
-            "cargo".into(),
-            "rustc".into(),
-            "rustdoc".into(),
-            "rustfmt".into(),
-            "rustup".into(),
-        ],
-        lock_file_name: Some("Cargo.lock".into()),
-        manifest_file_name: Some("Cargo.toml".into()),
-        vendor_dir_name: Some("target".into()),
-        ..Default::default()
-    }))
-}
-
-#[plugin_fn]
-pub fn define_toolchain_config() -> FnResult<Json<DefineToolchainConfigOutput>> {
-    Ok(Json(DefineToolchainConfigOutput {
-        schema: SchemaBuilder::build_root::<RustToolchainConfig>(),
-    }))
-}
-
-#[plugin_fn]
-pub fn initialize_toolchain(
-    Json(_): Json<InitializeToolchainInput>,
-) -> FnResult<Json<InitializeToolchainOutput>> {
-    Ok(Json(InitializeToolchainOutput {
-        config_url: Some("https://moonrepo.dev/docs/guides/rust/handbook".into()),
-        docs_url: Some("https://moonrepo.dev/docs/config/toolchain#rust".into()),
-        prompts: vec![], // TODO
-        ..Default::default()
-    }))
-}
-
-#[plugin_fn]
 pub fn extend_project_graph(
     Json(input): Json<ExtendProjectGraphInput>,
 ) -> FnResult<Json<ExtendProjectGraphOutput>> {
