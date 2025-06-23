2e0ffb037e0f3ee123f30e1348bde6b4cc2da14c
Miles Johnson <mileswjohnson@gmail.com>
2025-06-23 16:16:25 -0700

    Update gomod parser.

--- toolchains/go/src/go_mod.rs
+++ toolchains/go/src/go_mod.rs
@@ -4,7 +4,7 @@ use go_tool::version::from_go_version;
 use moon_pdk_api::{AnyResult, anyhow};
 use std::str::FromStr;
 
-pub use gomod_parser::*;
+pub use gomod_parser2::*;
 
 pub fn parse_go_mod(content: impl AsRef<str>) -> AnyResult<GoMod> {
     let mut go_mod = GoMod::from_str(content.as_ref()).map_err(|error| anyhow!("{error}"))?;
