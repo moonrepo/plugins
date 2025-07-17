a2f6d13e9ded473ddfb90ec1ed2efd34b21a9655
Miles Johnson <mileswjohnson@gmail.com>
2025-07-17 15:43:45 -0700

    Add tests.

--- toolchains/go/tests/go_work_test.rs
+++ toolchains/go/tests/go_work_test.rs
@@ -16,6 +16,18 @@ mod go_work {
     }
 
     #[test]
+    fn parses_basic_without_trailing() {
+        let sandbox = create_sandbox("work-files");
+
+        GoWork::parse(
+            fs::read_to_string(sandbox.path().join("basic.work"))
+                .unwrap()
+                .trim(),
+        )
+        .unwrap();
+    }
+
+    #[test]
     fn parses_advanced() {
         let sandbox = create_sandbox("work-files");
         let go_work =
@@ -24,5 +36,17 @@ mod go_work {
 
         assert_eq!(go_work.version.unwrap(), "1.23.0");
         assert_eq!(go_work.modules, vec!["a", "b", "d", ".", "e", "f"]);
+    }
+
+    #[test]
+    fn parses_advanced_without_trailing() {
+        let sandbox = create_sandbox("work-files");
+
+        GoWork::parse(
+            fs::read_to_string(sandbox.path().join("advanced.work"))
+                .unwrap()
+                .trim(),
+        )
+        .unwrap();
     }
 }
