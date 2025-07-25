6f779b6d40a0a39a63ead90b199a8cca999f92e9
Miles Johnson <mileswjohnson@gmail.com>
2025-07-25 09:59:56 -0700

    Update deps.

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,6 +1,11 @@
-use proto_pdk::{WarpgateTracingOptions, initialize_tracing_with_options};
+use proto_pdk::{WarpgateTracingOptions, get_test_environment, initialize_tracing_with_options};
 
 pub fn enable_tracing() {
+    // Disable tracing while running tests
+    if let Ok(Some(_)) = get_test_environment() {
+        return;
+    }
+
     initialize_tracing_with_options(WarpgateTracingOptions {
         modules: vec![
             "moon".into(),
@@ -10,5 +15,5 @@ pub fn enable_tracing() {
             "typescript_tsconfig_json".into(),
         ],
         ..Default::default()
-    })
+    });
 }
