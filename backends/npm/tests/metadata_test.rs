f79dd99637b99a609024472a322d577020ccfcd1
Miles Johnson <mileswjohnson@gmail.com>
2025-09-08 15:14:57 -0700

    Start on npm backend.

---
+++ backends/npm/tests/metadata_test.rs
@@ -0,0 +1,36 @@
+#[cfg(not(windows))]
+mod asdf_backend {
+    use proto_pdk_test_utils::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn registers_metadata() {
+        let sandbox = create_empty_proto_sandbox();
+        let plugin = sandbox.create_plugin("zig").await;
+
+        let metadata = plugin
+            .register_tool(RegisterToolInput { id: "zig".into() })
+            .await;
+
+        assert_eq!(metadata.name, "asdf:zig");
+        assert_eq!(
+            metadata.plugin_version.unwrap().to_string(),
+            env!("CARGO_PKG_VERSION")
+        );
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn registers_backend() {
+        let sandbox = create_empty_proto_sandbox();
+        let plugin = sandbox.create_plugin("zig").await;
+
+        let metadata = plugin
+            .register_backend(RegisterBackendInput::default())
+            .await;
+
+        assert_eq!(metadata.backend_id, "zig");
+
+        if let SourceLocation::Git(git) = metadata.source.unwrap() {
+            assert_eq!(git.url, "https://github.com/cheetah/asdf-zig");
+        }
+    }
+}
