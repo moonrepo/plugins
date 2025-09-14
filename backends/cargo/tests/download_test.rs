7b980d345f474372f25992bea737ed99f4aaecc8
Miles Johnson <mileswjohnson@gmail.com>
2025-09-14 12:58:57 -0700

    Add more tests.

--- backends/cargo/tests/download_test.rs
+++ backends/cargo/tests/download_test.rs
@@ -1,4 +1,4 @@
-use cargo_backend::CargoBackendConfig;
+use cargo_backend::{CargoBackendConfig, CargoToolConfig};
 use proto_pdk_test_utils::*;
 
 mod cargo_backend_download {
@@ -16,6 +16,42 @@ mod cargo_backend_download {
         generate_native_install_tests!("cargo:eza", "0.23.1", None, |cfg| {
             cfg.backend_config(CargoBackendConfig {
                 no_binstall: true,
+                ..Default::default()
+            });
+        });
+    }
+
+    // https://github.com/kbknapp/cargo-outdated/blob/master/Cargo.toml
+
+    mod features {
+        use super::*;
+
+        generate_native_install_tests!("cargo:cargo-outdated", "0.17.0", None, |cfg| {
+            cfg.tool_config(CargoToolConfig {
+                features: vec!["debug".into()],
+                no_default_features: true,
+                ..Default::default()
+            });
+        });
+    }
+
+    mod bin {
+        use super::*;
+
+        generate_native_install_tests!("cargo:cargo-outdated", "0.17.0", None, |cfg| {
+            cfg.tool_config(CargoToolConfig {
+                bin: Some("cargo-outdated".into()),
+                ..Default::default()
+            });
+        });
+    }
+
+    mod git {
+        use super::*;
+
+        generate_native_install_tests!("cargo:cargo-outdated", "0.17.0", None, |cfg| {
+            cfg.tool_config(CargoToolConfig {
+                git_url: Some("https://github.com/kbknapp/cargo-outdated.git".into()),
                 ..Default::default()
             });
         });
