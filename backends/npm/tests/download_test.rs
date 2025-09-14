7d2a339f51825af3ac83dae91aa13f555a640e16
Miles Johnson <mileswjohnson@gmail.com>
2025-09-13 20:22:58 -0700

    Polish.

--- backends/npm/tests/download_test.rs
+++ backends/npm/tests/download_test.rs
@@ -1,7 +1,20 @@
+use npm_backend::NpmBackendConfig;
 use proto_pdk_test_utils::*;
 
-mod npm_backend {
+mod npm_backend_download {
     use super::*;
 
-    generate_native_install_tests!("npm:typescript", "5.9.2");
+    mod with_node {
+        use super::*;
+
+        generate_native_install_tests!("npm:typescript", "5.9.2");
+    }
+
+    mod with_bun {
+        use super::*;
+
+        generate_native_install_tests!("npm:typescript", "5.9.2", None, |cfg| {
+            cfg.backend_config(NpmBackendConfig { bun: true });
+        });
+    }
 }
