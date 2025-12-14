0808f6adf22a662f3f82cab968495c502124a410
Miles Johnson <mileswjohnson@gmail.com>
2025-12-13 23:00:18 -0800

    Add lock parsing.

---
+++ toolchains/python/src/managers/pip.rs
@@ -0,0 +1,96 @@
+use moon_config::UnresolvedVersionSpec;
+use moon_pdk::{AnyResult, VirtualPath};
+use moon_pdk_api::{LockDependency, ParseLockOutput};
+use pep508_rs::{Requirement, VerbatimUrl, VersionOrUrl};
+use serde::{Deserialize, Serialize};
+use starbase_utils::{fs, toml};
+use std::collections::HashMap;
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+pub fn parse_requirements_txt(path: &VirtualPath, output: &mut ParseLockOutput) -> AnyResult<()> {
+    let file = fs::open_file(&path)?;
+
+    for line in io::BufReader::new(file).lines().map_while(Result::ok) {
+        if let Ok(parsed) = Requirement::<VerbatimUrl>::from_str(&line) {
+            let mut dep = LockDependency::default();
+
+            if let Some(VersionOrUrl::VersionSpecifier(spec)) = parsed.version_or_url {
+                dep.req = UnresolvedVersionSpec::parse(spec.to_string()).ok();
+            }
+
+            if !parsed.extras.is_empty() {
+                dep.meta = Some(
+                    parsed
+                        .extras
+                        .into_iter()
+                        .map(|e| e.to_string())
+                        .collect::<Vec<_>>()
+                        .join(","),
+                );
+            }
+
+            output
+                .dependencies
+                .entry(parsed.name.to_string())
+                .or_default()
+                .push(dep);
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct PyLockPackageDist {
+    pub url: String,
+    pub hashes: HashMap<String, String>,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct PyLockPackage {
+    pub archive: Option<PyLockPackageDist>,
+    pub name: String,
+    pub version: Option<String>,
+    pub sdist: Option<PyLockPackageDist>,
+}
+
+// https://peps.python.org/pep-0751/
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct PyLock {
+    pub extras: Vec<String>,
+    pub lock_version: Option<String>,
+    pub packages: Vec<PyLockPackage>,
+    pub requires_python: Option<String>,
+}
+
+pub fn parse_pylock_toml(path: &VirtualPath, output: &mut ParseLockOutput) -> AnyResult<()> {
+    let content = fs::read_file(path)?;
+    let lock: PyLock = toml::parse(&content)?;
+
+    for package in lock.packages {
+        // TODO version
+        let mut dep = LockDependency::default();
+
+        if let Some(archive) = package.archive {
+            if let Some(hash) = archive.hashes.get("sha256") {
+                dep.hash = Some(hash.to_owned());
+            }
+        } else if let Some(sdist) = package.sdist {
+            if let Some(hash) = sdist.hashes.get("sha256") {
+                dep.hash = Some(hash.to_owned());
+            }
+        }
+
+        output
+            .dependencies
+            .entry(package.name)
+            .or_default()
+            .push(dep);
+    }
+
+    Ok(())
+}
---
+++ toolchains/python/src/managers/uv.rs
@@ -0,0 +1,53 @@
+use moon_config::UnresolvedVersionSpec;
+use moon_pdk::{AnyResult, VirtualPath};
+use moon_pdk_api::{LockDependency, ParseLockOutput};
+use serde::{Deserialize, Serialize};
+use starbase_utils::{fs, toml};
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct UvLockPackageSdist {
+    pub url: String,
+    pub hash: String,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct UvLockPackage {
+    pub name: String,
+    pub version: String,
+    pub sdist: UvLockPackageSdist,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(default, rename_all = "kebab-case")]
+pub struct UvLock {
+    pub package: Vec<UvLockPackage>,
+    pub requires_python: Option<String>,
+    pub revision: Option<u8>,
+    pub version: Option<u8>,
+}
+
+pub fn parse_uv_lock(path: &VirtualPath, output: &mut ParseLockOutput) -> AnyResult<()> {
+    let content = fs::read_file(path)?;
+    let lock: UvLock = toml::parse(&content)?;
+
+    for package in lock.package {
+        output
+            .dependencies
+            .entry(package.name)
+            .or_default()
+            .push(LockDependency {
+                // We use `UnresolvedVersionSpec` here because versions in uv
+                // lockfiles may have missing minor/patch parts
+                version: match UnresolvedVersionSpec::parse(package.version) {
+                    Ok(spec) => Some(spec.to_resolved_spec()),
+                    Err(_) => None,
+                },
+                hash: Some(package.sdist.hash),
+                ..Default::default()
+            });
+    }
+
+    Ok(())
+}
