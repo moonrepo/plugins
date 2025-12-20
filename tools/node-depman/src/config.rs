f40b868a00909bc1410360f50d5afc7146974c2a
Nicolas Le Cam <niko.lecam@gmail.com>
2025-12-20 17:10:27 +0100

    new: Allow configuring default `node` package managers registry

--- tools/node-depman/src/config.rs
+++ tools/node-depman/src/config.rs
@@ -1,6 +1,7 @@
 #[derive(Debug, schematic::Schematic, serde::Deserialize, serde::Serialize)]
 #[serde(default, deny_unknown_fields, rename_all = "kebab-case")]
 pub struct NodeDepmanToolConfig {
+    pub registry_url: String,
     pub dist_url: String,
     pub shared_globals_dir: bool,
 }
@@ -8,9 +9,8 @@ pub struct NodeDepmanToolConfig {
 impl Default for NodeDepmanToolConfig {
     fn default() -> Self {
         Self {
-            dist_url:
-                "https://registry.npmjs.org/{package}/-/{package_without_scope}-{version}.tgz"
-                    .into(),
+            registry_url: "https://registry.npmjs.org".into(),
+            dist_url: "{registry}/{package}/-/{package_without_scope}-{version}.tgz".into(),
             shared_globals_dir: false,
         }
     }
